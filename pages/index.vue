319390dfeafe90596318b65d9c491e737f156124
Ola <ola.frankowska@snow.dog>
2021-03-12 12:03:01 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col justify-center items-center w-full min-h-screen">
+    <a-heading
+      tag="h1"
+      :variant="['first-level', 'font-secondary']"
+      class="mb-4"
+    >
+      Alpaca UI
+    </a-heading>
+    <a-heading
+      tag="h2"
+      :variant="['second-level']"
+      class="mb-4"
+    >
+      Nuxt Playground
+    </a-heading>
+    <a-price
+      regular-price="$100,00"
+      class="mb-4"
+    />
+    <a-price
+      regular-price="$36,00"
+      special-price="$19,90"
+      aria-label-special-price="Price reduced from:"
+      aria-label-old-price="On sale at:"
+    />
+  </div>
+</template>
+
+<script>
+import { Heading, Price } from '@snowdog/alpaca-ui'
+export default {
+  components: {
+    AHeading: Heading,
+    APrice: Price
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+
+const alpaca = require('@snowdog/alpaca-ui/tailwind.config.js')
+
+module.exports = {
+  theme: {
+    ...alpaca.theme,
+    textColor: theme => ({
+      ...theme('colors'),
+      default: theme('colors.gray.800'),
+      primary: 'red'
+    })
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
+}
